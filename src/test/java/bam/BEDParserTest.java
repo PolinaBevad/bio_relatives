27b299e05e42abb66dbe31e3baff993a42c933c3
ishvatov <ishvatov@gmail.com>
2019-03-21 17:49:30 +0300

    Commit by Sergey Hvatov on 21.03
    - Imporved BEDParser
    - Improved and added some tests for BEDParser class
    - AAdded new bed files for testiong

--- src/test/java/bam/BEDParserTest.java
+++ src/test/java/bam/BEDParserTest.java
@@ -21,37 +21,48 @@ public class BEDParserTest
     /**
      * Path to file with wrong extension.
      */
-    private static final String pathToFileWithWrongExt = "/home/ishvatov/Desktop/bio_relatives/src/test/resources/bam/BEDParser/file.txt";
+    private static final String pathToFileWithWrongExt = "C:\\Users\\Сергей\\Documents\\GitHub\\bio_relatives\\src\\test\\resources\\bam\\BEDParser\\file.txt";
 
     /**
-     * Path to incorrect bed file .
+     * Path to incorrect bed file with wrong start and end position.
      */
-    private static final String pathToIncorrectFile = "/home/ishvatov/Desktop/bio_relatives/src/test/resources/bam/BEDParser/incorrect.bed";
+    private static final String pathToIncorrectFile1 = "C:\\Users\\Сергей\\Documents\\GitHub\\bio_relatives\\src\\test\\resources\\bam\\BEDParser\\incorrect1.bed";
+
+    /**
+     * Path to incorrect bed file with wrong genome name.
+     */
+    private static final String pathToIncorrectFile2 = "C:\\Users\\Сергей\\Documents\\GitHub\\bio_relatives\\src\\test\\resources\\bam\\BEDParser\\incorrect2.bed";
 
     /**
      * Path to correct bed file.
      */
-    private static final String pathToCorrectFile = "/home/ishvatov/Desktop/bio_relatives/src/test/resources/bam/BEDParser/correct.bed";
+    private static final String pathToCorrectFile = "C:\\Users\\Сергей\\Documents\\GitHub\\bio_relatives\\src\\test\\resources\\bam\\BEDParser\\correct.bed";
 
     /**
      * Path to directory.
      */
-    private static final String pathToNotAFile = "/home/ishvatov/Desktop/bio_relatives/src/test/resources/bam/BEDParser/";
+    private static final String pathToNotAFile = "C:\\Users\\Сергей\\Documents\\GitHub\\bio_relatives\\src\\test\\resources\\bam\\BEDParser\\";
 
     /**
      * Correct start position values from the file.
      */
-    private static final int[] correctStart = {127471196, 127472363, 127473530, 127474697, 127475864, 127477031};
+    private static final int[] correctStart = {169853073, 169795048, 27612063, 196651877, 24357004, 24415793, 33013043, 23019447, 54784550};
 
     /**
      * Correct en position values from the file.
      */
-    private static final int[] correctEnd = {127472363, 127473530, 127474697, 127475864, 127477031, 127478198};
+    private static final int[] correctEnd = {169893959, 169854080, 27635277, 196747504, 24413725, 24469307, 33036992, 23083689, 54801198};
 
     /**
      * Correct name of the chromosome from the file.
      */
-    private static final String correctName = "chr7";
+    private static final String correctName = "chr1";
+
+    /**
+     * Correct names of the genomes from the file.
+     */
+    private static final String[] correctGenome = {"SCYL3", "C1orf112", "FGR", "CFH", "STPG1", "NIPAL3", "AK2", "KDM1A", "TTC22"};
+
 
     @Test(expected = InvalidBEDFileException.class)
     public void CreationFromPathToNonExistentFile() throws Exception
@@ -78,9 +89,16 @@ public class BEDParserTest
     }
 
     @Test(expected = InvalidBEDFileException.class)
-    public void ParsingIncorrectFile() throws Exception
+    public void ParsingIncorrectFileWithWrongStartEnd() throws Exception
     {
-        BEDParser parser = new BEDParser(pathToIncorrectFile);
+        BEDParser parser = new BEDParser(pathToIncorrectFile1);
+        ArrayList<BEDParser.BEDFeature> result = parser.parse();
+    }
+
+    @Test(expected = InvalidBEDFileException.class)
+    public void ParsingIncorrectFileWithWrongGenome() throws Exception
+    {
+        BEDParser parser = new BEDParser(pathToIncorrectFile2);
         ArrayList<BEDParser.BEDFeature> result = parser.parse();
     }
 
@@ -94,6 +112,7 @@ public class BEDParserTest
             assertEquals(result.get(i).getStartPos(), correctStart[i]);
             assertEquals(result.get(i).getEndPos(), correctEnd[i]);
             assertEquals(result.get(i).getChromosomeName(), correctName);
+            assertEquals(result.get(i).getGenomeSymbol(), correctGenome[i]);
         }
     }
 }

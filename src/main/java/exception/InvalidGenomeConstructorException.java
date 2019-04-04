d69dfca82d302d31e08cfd35a3cbd02dc886a1d1
MarchenkoVladislav <vlad25091999@mail.ru>
2019-04-04 19:40:59 +0300

    Adding a genome assembly

---
+++ src/main/java/exception/InvalidGenomeConstructorException.java
@@ -0,0 +1,12 @@
+package exception;
+
+/**
+ * Defines a basic exception that may occur during the initialization
+ * of the {@link genome.assembly.GenomeConstructor} object.
+ * @author Vladislav Marchenko
+ */
+public class InvalidGenomeConstructorException extends Exception {
+    public InvalidGenomeConstructorException(String method, String wrongField, String mes) {
+        super("Error occurred in method [" + method + "]: field [" + wrongField + "] is [" + mes + "].");
+    }
+}
--- src/main/java/genome/assembly/GenomeConstructor.java
+++ src/main/java/genome/assembly/GenomeConstructor.java
@@ -1,10 +1,12 @@
 package genome.assembly;
 
+import bam.BEDParser;
+import exception.InvalidGenomeAssemblyException;
+import exception.InvalidGenomeConstructorException;
+import exception.InvalidRegionException;
 import htsjdk.samtools.SAMRecord;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * This class implements {@link GenomeAssembler} interface. It is designed to parse the
@@ -56,18 +58,113 @@ public class GenomeConstructor implements GenomeAssembler
         } // cytosine
     }
 
+    /**
+     * input ArrayList of SamRecords from which we will construct a genome
+     */
+    public ArrayList<SAMRecord> samRecords;
+
+    /**
+     * input ArrayList of genome regions from BED file
+     */
+    public ArrayList<BEDParser.BEDFeature> exons;
+
+    /**
+     * Default constructor of class GenomeConstructor
+     * @param samRecords input ArrayList of SamRecords from which we will construct a genome
+     * @param exons input ArrayList of genome regions from BED file
+     * @throws InvalidGenomeConstructorException if input data is empty
+     */
+    public GenomeConstructor(ArrayList<SAMRecord> samRecords, ArrayList<BEDParser.BEDFeature> exons) throws InvalidGenomeConstructorException {
+        this.samRecords = samRecords;
+        if (samRecords.isEmpty()) {
+            throw new InvalidGenomeConstructorException("GenomeConstructor", "samRecords", "empty");
+        }
+
+        this.exons = exons;
+        if (exons.isEmpty()) {
+            throw new InvalidGenomeConstructorException("GenomeConstructor", "exons", "empty");
+        }
+    }
+
+    /**
+     * Assembly a genome from SAMRecords
+     * @return ArrayList of GenomeRegions(output genome)
+     * @throws InvalidGenomeAssemblyException if errors occur
+     */
     @Override
-    public List<GenomeRegion> assembly()
-    {
-        return new ArrayList<GenomeRegion>();
+    public List<GenomeRegion> assembly() throws InvalidGenomeAssemblyException {
+        try {
+            // output genome
+            List<GenomeRegion> genomeRegions = new ArrayList<>();
+            // we pass through each region from the BED file(each exon)
+            for (int i = 0; i < exons.size(); i++) {
+
+                // array of qualities of nucleotides from the current region
+                byte [] qualities = new byte[exons.get(i).getEndPos() - exons.get(i).getStartPos() + 1];
+                // String of nucleotides from the current region
+                StringBuilder nucleotides = new StringBuilder();
+
+                // we pass from start position to end position of current region
+                for (int j = exons.get(i).getStartPos(); j < exons.get(i).getEndPos(); j++) {
+                    // HashMap in which there are nucleotides(with their qualities; see description of the method)
+                    // from current position
+                    HashMap<Character, ArrayList<Byte>> currentNucleotides = getNucleotideDistribution(j);
+
+                    // the best nucleotide
+                    char bestNucleotide = NUCLEOTIDES.toCharArray()[0];
+                    // the best median quality of nucleotide
+                    byte bestQuality = 0;
+                    // the best count of nucleotides
+                    int bestCount = 0;
+
+                    // we pass on HashMap ( we define the best nucleotide)
+                    Set<Map.Entry<Character, ArrayList<Byte>>> set = currentNucleotides.entrySet();
+                    for (Map.Entry<Character, ArrayList<Byte>> s : set) {
+                        // if the current nucleotide is the most met then it is the best nucleotide
+                        if (s.getValue().size() > bestCount) {
+                            bestCount = s.getValue().size();
+                            bestNucleotide = s.getKey();
+                            bestQuality = getMedianQuality(s.getValue());
+                        }
+
+                        // if the current nucleotide occurs as many times as the best, then we look at their quality
+                        if (s.getValue().size() == bestCount) {
+                            // if the current nucleotide has the best quality, then it is the best
+                            if (getMedianQuality(s.getValue()) > bestQuality) {
+                                bestCount = s.getValue().size();
+                                bestNucleotide = s.getKey();
+                                bestQuality = getMedianQuality(s.getValue());
+                            }
+                        }
+                    }
+
+                    // add the best nucleotide into the nucleotide siquence from the current region
+                    nucleotides.append(bestNucleotide);
+                    // add the quality of this nucleotide
+                    qualities[j - exons.get(i).getStartPos()] = bestQuality;
+                }
+
+                // create an GenomeRegion object
+                GenomeRegion genomeRegion = new GenomeRegion(exons.get(i).getChromosomeName(), exons.get(i).getStartPos(),
+                        nucleotides.toString().toUpperCase(), qualities);
+                // add this region into output List
+                genomeRegions.add(genomeRegion);
+            }
+            return genomeRegions;
+        } catch (NullPointerException | InvalidRegionException | IllegalArgumentException ex ) {
+            // if catch an exception then create our InvalidGenomeAssemblyException exception,
+            InvalidGenomeAssemblyException ibfex = new InvalidGenomeAssemblyException(ex.getMessage());
+            ibfex.initCause(ex);
+            throw ibfex;
+        }
     }
 
     /**
      * Checks if the current position is in the range [start position of the
      * nucleotide; start position + nucleotide sequence len]. Using this method we
-     * check, if the {@link htsjdk.samtools.SAMRecord} contains current processing nucleotide
+     * check, if the {@link SAMRecord} contains current processing nucleotide
      * @param position Position of the current nucleotide.
-     * @param start Start position of the nucleotide in the {@link htsjdk.samtools.SAMRecord}
+     * @param start Start position of the nucleotide in the {@link SAMRecord}
      * @param end End psoition of the nucleotide sequence.
      * @return True, if position is in range [start, start + len]. False otherwise.
      */
@@ -78,11 +175,10 @@ public class GenomeConstructor implements GenomeAssembler
 
     /**
      * Generates a map with each nucleotide and it's quality for the further usage.
-     * @param reads Array of {@link SAMRecord} read from BAM file.
      * @param position Current position of the nucleotide we are analyzing.
      * @return HashMap with qualities for this nucleotide.
      */
-    private HashMap<Character, ArrayList<Byte>> getNucleotideDistribution(ArrayList<SAMRecord> reads, int position)
+    private HashMap<Character, ArrayList<Byte>> getNucleotideDistribution(int position)
     {
         // initialize the storing structure
         HashMap<Character, ArrayList<Byte>> dist = new HashMap<>();
@@ -92,7 +188,7 @@ public class GenomeConstructor implements GenomeAssembler
         }
         // for each read get the
         // nucleotide and it's quality if it contains it
-        reads.forEach(s ->
+        samRecords.forEach(s ->
         {
             if (!inRange(position, s.getStart(), s.getEnd()))
             {
@@ -103,5 +199,25 @@ public class GenomeConstructor implements GenomeAssembler
             }
         });
         return dist;
+    }
+
+    /**
+     * method for definition of median quality of the nucleotide
+     * @param qualities input array of qualities of the nucleotide
+     * @return median quality of the nucleotide
+     */
+    private byte getMedianQuality (ArrayList<Byte> qualities) {
+        // sort ArrayList of qualities
+        Collections.sort(qualities);
+
+        // if the number of elements is odd, then we take the middle element
+        if (qualities.size() % 2 != 0) {
+            return qualities.get(qualities.size() / 2);
+        }
+
+        // else return half of the sum of the two middle elements of the array
+        else {
+            return (byte) ((qualities.get(qualities.size() / 2) + qualities.get(qualities.size() - 1)) / 2);
+        }
     }
 }
